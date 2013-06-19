38315a3169055b4b0667b6f2faa4458f4204b6b7
Andrew Ambrosino <andrew@ajambrosino.com>
2013-06-19 15:56:28 -0700

    UI Awesomeness

--- js/user.js
+++ js/user.js
@@ -5,9 +5,23 @@
 // class CommunicationObject 
 WBR.UserController = Ember.ArrayController.create({
      content : [{name:"ajambrosino",id:"sda3", tx:true}],
+
+     setTx: function(e) {
+     	var id = $(e).find(".user-cell-id").text();
+  		WBR.Room.setTx(id);
+
+  		for (i = 0; i < this.content.length; i++) {
+  			if (this.content[i].id == id)
+  				this.content[i].tx = true;
+  			else
+  				this.content[i].tx = false;
+  		}
+
+  		$('.user-cell').removeClass('enabled-user');
+  		$(e).addClass('enabled-user');
+     }
+
 })
-
-
 
 WBR.ApplicationView = Ember.View.create({
       templateName: 'table'
