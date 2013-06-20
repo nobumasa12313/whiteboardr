c5a7ca84f514dcaa7d6a29a9d22c2af4c54ebb29
Heemin Seog <seog.heemin@gmail.com>
2013-06-20 16:49:04 -0700

    video files

---
+++ js/build.js
@@ -0,0 +1,13 @@
+var request = require('request'),
+    fs = require('fs'),
+    uglify = require('uglify-js');
+
+var source = fs.readFileSync('simplewebrtc.js').toString();
+
+request.get('http://johnamoore.com:8888/socket.io/socket.io.js', function (err, res, body) {
+    if (!err && body && body.length) {
+        fs.writeFile('latest.js', uglify.minify(source + body, {fromString: true}).code, function (err) {
+            if (err) throw err;
+        });
+    }
+});
---
+++ js/video.js
@@ -0,0 +1,43 @@
+// grab the room from the URL
+var room = location.search && location.search.split('?')[1];
+
+// create our webrtc connection
+var webrtc = new WebRTC({
+    // the id/element dom element that will hold "our" video
+    localVideoEl: 'localVideo',
+    // the id/element dom element that will hold remote videos
+    remoteVideosEl: 'remotes',
+    // immediately ask for camera access
+    autoRequestMedia: true,
+    log: true
+});
+
+// when it's ready, join if we got a room from the URL
+webrtc.on('readyToCall', function () {
+    // you can name it anything
+    if (room) webrtc.joinRoom(room);
+});
+
+// Since we use this twice we put it here
+function setRoom(name) {
+    $('form').remove();
+    $('h1').text(name);
+    $('#subTitle').text('Link to join: ' + location.href);
+    $('body').addClass('active');
+}
+
+if (room) {
+    setRoom(room);
+} else {
+    $('form').submit(function () {
+        var val = $('#sessionInput').val().toLowerCase().replace(/\s/g, '-').replace(/[^A-Za-z0-9_\-]/g, '');
+        webrtc.createRoom(val, function (err, name) {
+            var newUrl = location.pathname + '?' + name;
+            if (!err) {
+                history.replaceState({foo: 'bar'}, null, newUrl);
+                setRoom(name);
+            }
+        });
+        return false;          
+    });
+}
