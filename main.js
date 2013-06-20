c55ca45de634a7c78bddbee779ce84c937f57908
Heemin Seog <seog.heemin@gmail.com>
2013-06-20 10:23:45 -0700

    main.js and manifest.json

---
+++ main.js
@@ -0,0 +1,23 @@
+chrome.app.runtime.onLaunched.addListener(function() {
+	//what to launch and how
+	chrome.app.window.create('index.html', {
+		bounds: {
+			width:800,
+			height:600,
+			left:100,
+			top:100
+		},
+		minWidth: 800,
+		minHeight:600
+	});
+});
+
+chrome.runtime.onInstalled.addListener(function() {
+	//database
+	//chrome.storage.local.set(object items, function callback);
+});
+
+chrome.runtime.onSuspend.addListener(function() {
+	//close open connections.
+	//do cleanup tasks
+})
