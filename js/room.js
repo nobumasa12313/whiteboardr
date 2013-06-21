d9e01601cf48b992721081ca4279d910f43cc657
John Moore <john@programiscellaneous.com>
2013-06-21 03:30:19 -0700

    bugfix

--- js/room.js
+++ js/room.js
@@ -209,11 +209,11 @@ settxMessageListener: function(fromClientID, datastr) {
   //alert(datastr);
   WBR.Room.adminID = fromClientID;
     WBR.Room.tx = parseInt(datastr);
-    if (datastr == WBR.Room.orbiter.clientID) {
+    if (WBR.Room.tx == WBR.Room.orbiter.clientID) {
       //We're the tx!
       WBR.Room.transmitSerial();
       if (WBR.Room.broadcast == true && WBR.Room.tx == WBR.Room.orbiter.clientID) {
-      		WBR.Canvas.adminCommandCache = WBR.Canvas.userCommandCache;
+      		WBR.Canvas.adminCommandCache = JSON.parse(JSON.stringify(WBR.Canvas.userCommandCache));
 	if (WBR.Room.admincanvas == true) {
 		 WBR.Canvas.currentCanvas.getContext('2d').clearRect(0, 0, WBR.Canvas.currentCanvas.width, WBR.Canvas.currentCanvas.height);
  
