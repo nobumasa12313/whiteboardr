8e52c1995ae59242550f92febc303a78b8ea2b98
John Moore <john@programiscellaneous.com>
2013-06-21 03:55:22 -0700

    bug

--- js/room.js
+++ js/room.js
@@ -808,8 +808,13 @@ clientAttributeUpdateListener: function(attrScope,
 			if (WBR.Room.tx == WBR.Room.orbiter.clientID) {
 				if (WBR.Room.admincanvas == false) {
 				WBR.Canvas.userCommands[clientID].push(command);
-				}
 				WBR.Canvas.userCommandCache[clientID].push(command);
+				WBR.Canvas.adminCommandCache[clientID].push(command);
+				} else {
+					WBR.Canvas.adminCommandCache[clientID].push(command);
+					WBR.Canvas.userCommands[clientID].push(command);
+				}
+				
 				//WBR.Canvas.adminCommandCache[clientID].push(command);
 
 			} else {
